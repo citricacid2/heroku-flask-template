31f3a20599a29ddde2dc74cd425aabea48ada4d3
citricacid2 <61765149+citricacid2@users.noreply.github.com>
2020-05-20 15:49:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template
+import json
+import os
 
+from flask import Flask, request, send_file
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/testpost/', methods=['GET', 'POST'])
+def testpost():
+    if len(request.args) == 1:
+        with open('test.json') as f:
+            data = json.load(f)
+            for i in request.args:
+                data['history'].append(request.args[i])
+        os.remove('test.json')
+        with open('test.json', 'w') as f:
+            json.dump(data, f, indent=4)
+    return send_file('test.json')
 
 
-
-
-
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port=80)
